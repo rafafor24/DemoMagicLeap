03e951d2ba191add47c6aadc24ca424ec1c103bb
DESKTOP-3RRHD7P\User <ra.forero11@uniandes.edu.co>
2019-03-05 17:22:42 -0500

    Commit Inicial

---
+++ Assets/Scripts/AudioController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+     using System.Collections;
+ 
+     [RequireComponent(typeof(AudioSource))]
+public class AudioController : MonoBehaviour
+{
+    public AudioClip engineStartClip;
+    public AudioClip engineLoopClip;
+    void Start()
+    {
+        GetComponent<AudioSource>().loop = true;
+        StartCoroutine(playEngineSound());
+    }
+
+    IEnumerator playEngineSound()
+    {
+        GetComponent<AudioSource>().clip = engineStartClip;
+        GetComponent<AudioSource>().Play();
+        yield return new WaitForSeconds(GetComponent<AudioSource>().clip.length);
+        GetComponent<AudioSource>().clip = engineLoopClip;
+        GetComponent<AudioSource>().Play();
+    }
+}
---
+++ Assets/Scripts/MoveCloser.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.MagicLeap;
+
+public class MoveCloser : MonoBehaviour
+{
+
+
+    #region Private Variables  
+    [SerializeField, Tooltip("KeyPose to track.")]
+    private MLHandKeyPose _keyPoseToTrack;
+
+    [Space, SerializeField, Tooltip("Flag to specify if left hand should be tracked.")]
+    private bool _trackLeftHand = true;
+
+    [SerializeField, Tooltip("Flag to specify id right hand should be tracked.")]
+    private bool _trackRightHand = true;
+    
+    private float speed=50.0f;
+    private Camera _camera;
+    #endregion
+
+    #region Unity Methods
+    void Awake()
+    {
+        _camera = Camera.main;
+    }
+
+    void Update()
+    {
+
+        float confidenceLeft = _trackLeftHand ? GetKeyPoseConfidence(MLHands.Left) : 0.0f;
+        float confidenceRight = _trackRightHand ? GetKeyPoseConfidence(MLHands.Right) : 0.0f;
+        float confidenceValue = Mathf.Max(confidenceLeft, confidenceRight);
+
+        
+
+        
+
+        if (confidenceValue > 0.5f)
+        {
+            //vec.x = Input.GetAxis("Horizontal") * 25 * Time.deltaTime;
+            //vec.z = Input.GetAxis("Vertical") * 25 * Time.deltaTime;
+            //vec = Camera.main.transform.TransformDirection(vec.x, 0, vec.z);
+            //force = vec.normalized * speed;
+            GetComponent<Rigidbody>().AddForce(Vector3.Reflect(_camera.gameObject.transform.forward,Vector3.right) * speed);
+        }
+
+    }
+    #endregion
+
+    #region Private Methods
+    /// <summary>
+    /// Get the confidence value for the hand being tracked.
+    /// </summary>
+    /// <param name="hand">Hand to check the confidence value on. </param>
+    /// <returns></returns>
+    private float GetKeyPoseConfidence(MLHand hand)
+    {
+        if (hand != null)
+        {
+            if (hand.KeyPose == _keyPoseToTrack)
+            {
+                return hand.KeyPoseConfidence;
+            }
+        }
+        return 0.0f;
+    }
+    #endregion
+}
